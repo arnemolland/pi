289fc57e1de3cbef6902c81fbaabec89254b2cc7
Arne Molland <arne@molland.sh>
2025-04-28 12:57:23 +0200

    modules/personal: remove duplicate user definition

--- modules/personal/personal.nix
+++ modules/personal/personal.nix
@@ -1,10 +1,5 @@
 { config, ... }: {
   flake = {
     meta.personal.username = "arne";
-    modules.nixos.personal.users.users.${config.flake.meta.personal.username} =
-      {
-        isNormalUser = true;
-        initialPassword = "";
-      };
   };
 }
