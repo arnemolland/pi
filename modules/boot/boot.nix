0099565f39832a2bc31ca55ee351efed9c9fd5db
Arne Molland <arne@molland.sh>
2025-04-28 12:10:11 +0200

    Revert "modules/boot: drop into shell on failure"
    This reverts commit 1830f7adbcf8e9e389c84a608020bb79b23c6829.

--- modules/boot/boot.nix
+++ modules/boot/boot.nix
@@ -9,10 +9,7 @@
         systemd-boot.enable = true;
         systemd-boot.configurationLimit = 10;  # Optional: limit number of generations
       };
-
-      # Drop into a shell if boot fails
-      boot.kernelParams = [ "console=tty0" "console=ttyS0,115200" "boot.shell_on_fail" ];
-       
+      
       # Use a simple single boot partition config instead of redundancy
       fileSystems."/boot" = {
         device = "/dev/disk/by-label/boot";  # Use filesystem label
