64034892a6af4994416d8c37aa4fd08c9772dac8
Arne Molland <arne@molland.sh>
2025-04-28 13:59:59 +0200

    modules/personal: remove circular import

--- modules/personal/workstation/imports.nix
+++ modules/personal/workstation/imports.nix
@@ -1,7 +1,6 @@
 { config, ... }: {
   flake.modules.nixos."nixosConfigurations/workstation".imports =
     with config.flake.modules.nixos; [
-      desktop
       kde
       nvidia-gpu
       personal
